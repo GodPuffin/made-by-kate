eac01c717bfb1bfca7e63585babcc152c3f7a4cb
Marcus <mailingmarcuslee@gmail.com>
2024-07-26 12:01:50 -0400

    Spacing

--- medusajs-storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ medusajs-storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -23,7 +23,7 @@ export default function ProductRail({
         <LocalizedClientLink href={`/collections/${collection.handle}`}>
           <ScrollMarquee text={collection.title}/>
         </LocalizedClientLink>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-6">
+      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-10 gap-y-10">
         {products &&
           products.map((product) => (
             <li key={product.id}>
